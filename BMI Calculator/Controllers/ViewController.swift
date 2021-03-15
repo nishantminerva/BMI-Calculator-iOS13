64b7e1cf693be23eb6829688da1a3d66894cfb1e
Satyam Kumar <nishantcafe@gmaile.com>
2021-03-15 16:55:22 +0530

    ADDING CALCULATION FUNCTION

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,21 +12,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     
-    
-    @IBOutlet weak var weightLabel: UILabel!
-    
-    @IBAction func heightBarChanged(_ sender: UISlider) {
-        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
-    }
-    
-    @IBAction func weightBarChanged(_ sender: UISlider) {
-        weightLabel.text = "\(String(format: "%.0f", sender.value))kg"    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBAction func heightBarChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightBarChanged(_ sender: UISlider) {
+        weightLabel.text = "\(String(format: "%.0f", sender.value))Kg"    }
+    
+   
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 
 }
 
